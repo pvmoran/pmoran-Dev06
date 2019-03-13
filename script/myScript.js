27656c044ac7e6357c85335cc9b2a223b9308763
pvmoran <47060960+pvmoran@users.noreply.github.com>
2019-03-13 10:27:59 -0500

    Update myScript.js

--- script/myScript.js
+++ script/myScript.js
@@ -1,5 +1,12 @@
+<!-- Assign a local variable -->
 var name;
 
+<!-- Prompt the user to enter his or her name -->
 name = prompt("Welcome, " + name);
+
+<!-- Welcome the user to the assignment using a window alert. -->
+  
 alert("Hello " + name + "! Welcome to the Web Dev 06 - Input & Output Assignment.");
+
+<!-- Assign the same message that was used in the alert window to the console. -->
 console.log("Hello " + name + "! Welcome to the Web Dev 06 - Input & Output Assignment.");
